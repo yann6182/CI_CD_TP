f34518497d8d06bbf05b1b49de16c147697aa00b
yann6182 <yannickngaleu6@gmail.com>
2023-12-07 22:00:37 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+  return <div>
+
+
+    <header>
+        <h1>TIENDJEU NGALEU YANNICK</h1>
+        <p>Niveau : ING4ISI</p>
+    </header>
+
+    <main>
+        <p>Bienvenue sur la page de TIENDJEU NGALEU YANNICK!</p>
+        <p>Niveau : ING4ISI</p>
+    </main>
+
+
+
+  </div>
+}
+
+export default App
